78cb1ddfd9a75c07e2571556317f291361aa40a4
Leon Atherton <leon@idrsolutions.com>
2018-03-27 12:03:36 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# BuildVu JavaScript Client #
+
+The BuildVu JavaScript Client provides an easy way to try out IDRsolutions' [BuildVu Microservice Example](https://github.com/idrsolutions/buildvu-microservice-example).
+
+It functions as an easy to use library that lets you use [BuildVu](https://www.idrsolutions.com/buildvu/) from JavaScript.
+
+-----
+
+# Installation
+
+```
+<script src="path/to/buildvu-client.js" type="text/javascript"></script>
+```
+
+-----
+
+# Usage #
+
+```
+var baseEndpoint = "http://localhost:8080/microservice-example/";
+var endpoint = baseEndpoint + "buildvu";
+
+var file = document.getElementById('file-input').files[0];
+if (file.name) {
+    BuildVuClient.convert({
+        endpoint: endpoint,
+        file: file,
+        failure: function() { },
+        progress: function() { },
+        success: function(e) {
+            console.log("Converted " + baseEndpoint + e.previewPath);
+        }
+    });
+}
+```
+
+-----
+
+# Who do I talk to? #
+
+Found a bug, or have a suggestion / improvement? Let us know through the Issues page.
+
+Got questions? You can contact us [here](https://idrsolutions.zendesk.com/hc/en-us/requests/new).
+
+-----
+
+Copyright 2018 IDRsolutions
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+[http://www.apache.org/licenses/LICENSE-2.0](http://www.apache.org/licenses/LICENSE-2.0)
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
---
+++ buildvu-client.js
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2018 IDRsolutions
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+(function() {
+    window.BuildVuClient = (function() {
+
+        var progress, success, failure;
+
+        var doPoll = function(uuid, endpoint) {
+            var req, retries = 0;
+
+            var poll = setInterval(function () {
+                if (!req) {
+                    req = new XMLHttpRequest();
+                    req.onreadystatechange = function (e) {
+                        if (req.readyState === 4) {
+                            if (req.status === 200) {
+                                var data = JSON.parse(req.responseText);
+                                if (data.state === "processed") {
+                                    clearInterval(poll);
+                                    if (success) {
+                                        success(data);
+                                    }
+                                } else {
+                                    if (progress) {
+                                        progress(data);
+                                    }
+                                }
+                            } else {
+                                retries++;
+                                if (retries > 3) {
+                                    clearInterval(poll);
+                                    if (failure) {
+                                        failure("Connection error");
+                                    }
+                                }
+                            }
+                            req = null;
+                        }
+                    };
+                    req.open("GET", endpoint + "?uuid=" + uuid, true);
+                    req.send();
+                }
+            }, 500);
+        };
+
+        return {
+            convert: function(params) {
+                if (!params.endpoint) {
+                    throw Error('Missing endpoint');
+                }
+                if (!params.file || !params.file.name) {
+                    throw Error('Missing file');
+                }
+                if (params.success && typeof params.success === "function") {
+                    success = params.success;
+                }
+                if (params.failure && typeof params.failure === "function") {
+                    failure = params.failure;
+                }
+                if (params.progress && typeof params.progress === "function") {
+                    progress = params.progress;
+                }
+
+                var xhr = new XMLHttpRequest();
+                if (xhr.upload) {
+                    xhr.upload.addEventListener("progress", function(e) {
+                        if (progress) {
+                            progress({
+                                state: 'uploading',
+                                loaded: e.loaded,
+                                total: e.total
+                            });
+                        }
+                    }, false);
+
+                    xhr.onreadystatechange = function (e) {
+                        if (xhr.readyState === 4) {
+                            if (xhr.status === 200) {
+                                doPoll(JSON.parse(xhr.responseText).uuid, params.endpoint);
+                            } else {
+                                if (failure) {
+                                    failure("Connection error");
+                                }
+                            }
+                        }
+                    };
+
+                    xhr.open("POST", params.endpoint, true);
+                    var data = new FormData();
+                    data.append('file', params.file);
+                    if (params.parameters) {
+                        for (var prop in params.parameters) {
+                            if (params.parameters.hasOwnProperty(prop)) {
+                                data.append(prop, params.parameters[prop]);
+                            }
+                        }
+                    }
+                    xhr.send(data);
+                }
+            }
+        }
+    })();
+
+})();
